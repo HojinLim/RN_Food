c5c992e2f3028f91b276cfd29af1fb7321e31b55
HojinLim <hojinim@gmail.com>
2023-10-28 15:18:19 +0900

    Design/음식 아이템 디자인

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,13 +1,56 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, View, StyleSheet, Platform } from "react-native";
 
-function CategoryGridTile({title, color}){
-   return <View>
-    <Pressable>
-        <View>
-            <Text>{title}</Text>
+function CategoryGridTile({ title, color, onPress }) {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        onPress={onPress}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
-    </Pressable>
-   </View>
+      </Pressable>
+    </View>
+  );
 }
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 18,
+    height: 180,
+    elevation: 4,
+    borderRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    // ios
+    //  배경색도 지정해줘야함
+    backgroundColor: "white",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowColor: "black",
+    shadowOffset: { left: 2 },
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,19 +1,29 @@
 import { FlatList } from "react-native";
-import { CATEGORIES } from "../dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data";
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    const pressHandler = () => {
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
+    };
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
 
-function CategoriesScreen() {
   return (
     <FlatList
       data={CATEGORIES}
       keyExtractor={(item) => item.id}
       renderItem={renderCategoryItem}
+      numColumns="2"
     />
   );
 }
