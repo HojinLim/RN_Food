14dbe2b68902c1a87613f63d74c47343ad94fde2
HojinLim <hojinim@gmail.com>
2023-10-28 16:52:46 +0900

    Design/헤더 버튼

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,9 +14,30 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#ccc" },
+            headerTintColor: "brown",
+            contentStyle: { backgroundColor: "#cfc" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Meals Categories",
+            }}
+          />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+          <Stack.Screen
+            name="MealsDetail"
+            component={MealDetailScreen}
+            options={{
+              headerRight: () => {
+                <Button title="tap me!"/>
+              },
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
